eac8a606a0ddfcb44ec20582f7fceba69ab17f13
harsha-improve10X <shreeharsha@improve10x.com>
2024-06-22 14:38:12 +0530

    Added coroutines library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,8 @@ dependencies {
     implementation(libs.coil.compose)
     implementation (libs.retrofit)
     implementation (libs.converter.gson)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
