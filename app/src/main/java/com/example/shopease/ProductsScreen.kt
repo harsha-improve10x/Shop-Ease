adddab789b6f4de5493db786b2ae639f5c072b8d
harsha-improve10X <shreeharsha@improve10x.com>
2024-06-22 10:52:25 +0530

    Created a product screen

---
+++ app/src/main/java/com/example/shopease/ProductsScreen.kt
@@ -0,0 +1,19 @@
+package com.example.shopease
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Card
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ProductsScreen() {
+
+}
+
+@Composable
+fun ProductCard() {
+    Card() {
+        Column() {
+
+        }
+    }
+}
