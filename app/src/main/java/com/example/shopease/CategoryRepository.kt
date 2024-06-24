85e5d002caae6dc63823cbe1387a68a45378aa70
harsha-improve10X <shreeharsha@improve10x.com>
2024-06-24 22:20:04 +0530

    Worked on Category implementation

--- app/src/main/java/com/example/shopease/CategoryRepository.kt
+++ app/src/main/java/com/example/shopease/CategoryRepository.kt
@@ -1,7 +1,6 @@
 package com.example.shopease
 
 class CategoryRepository(private val apiService: FakeApiService) {
-
     suspend fun fetchCategories(): List<String> {
         return apiService.fetchCategories()
     }
--- app/src/main/java/com/example/shopease/categories/CategoryScreen.kt
+++ app/src/main/java/com/example/shopease/categories/CategoryScreen.kt
@@ -1,17 +1,35 @@
 package com.example.shopease.categories
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.shopease.CategoryRepository
+import com.example.shopease.CategoryViewModel
 
 @Composable
-fun CategoryScreen(category: Category) {
-
+fun CategoryScreen(categoryViewModel: CategoryViewModel) {
+    val categories by categoryViewModel.uiState.observeAsState(emptyList())
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(20.dp)
+        .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.Center
+    ) {
+        ButtonsList(categories)
+    }
 }
 
 @Composable
@@ -22,10 +40,11 @@ fun ActionButton(title: String) {
 }
 
 @Composable
-fun ButtonsList(categories: List<Category>) {
+fun ButtonsList(categories: List<String>) {
     Column(modifier = Modifier.fillMaxSize()) {
         categories.forEach { category ->
-            ActionButton(title = category.toString())
+            ActionButton(title = category)
+            Spacer(modifier = Modifier.height(10.dp))
         }
     }
 }
