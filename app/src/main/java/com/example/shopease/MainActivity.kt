b4c70f96fb0f65e511b0f0109a812beaf442d2bd
harsha-improve10X <shreeharsha@improve10x.com>
2024-06-24 22:22:19 +0530

    Removed greetings function

--- app/src/main/java/com/example/shopease/MainActivity.kt
+++ app/src/main/java/com/example/shopease/MainActivity.kt
@@ -30,18 +30,10 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ShopEaseTheme {
-        Greeting("Android")
+
     }
 }
